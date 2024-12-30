7e43b61b70aecf0c702e096d036c79a723101e24
pawel1909 <pawelpapiernik122@gmail.com>
2024-12-31 00:37:14 +0100

    zmiana

--- client/client.py
+++ client/client.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
 
-import time
 import urllib.request
 
 fp = urllib.request.urlopen("http://localhost:1234/")
 
-i = 0
+encodeContent = fp.read()
+decodedContent = encodeContent.decode("utf8")
 
-while i < 20:
-    encodeContent = fp.read()
-    decodedContent = encodeContent.decode("utf8")
+print(decodedContent)
 
-    print(decodedContent)
-    with open('data.txt', 'w') as f:
-        f.writelines(decodedContent)
-    time.sleep(15)
-    i += 1
 
 
 fp.close()
