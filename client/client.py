accf246cf7e9ec3a511cf2781ab016e037afdde9
pawel1909 <pawelpapiernik122@gmail.com>
2024-12-31 00:16:12 +0100

    dodanie gotowca z neta, żeby zobaczyć dlaczego nie chce mi działać

---
+++ client/client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import urllib.request
+
+fp = urllib.request.urlopen("http://localhost:1234/")
+
+encodeContent = fp.read()
+decodedContent = encodeContent.decode("utf8")
+
+print(decodedContent)
+
+fp.close()
