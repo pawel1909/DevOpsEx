3bb82cf161fdf57a4a615cc03ccc1dd6c20ed41d
pawel1909 <pawelpapiernik122@gmail.com>
2024-12-31 15:19:05 +0100

    test innego podejścia

---
+++ main_server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def route_request():
+    current_hour = datetime.now().hour
+    if 6 <= current_hour < 18:  # W godzinach 6-18 kierujemy na serwer A
+        response = requests.get('http://server_a:5000')
+    else:  # Poza tym czasem kierujemy na serwer B
+        response = requests.get('http://server_b:5000')
+    return response.text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
